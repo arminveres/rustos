38086bfbcc3214894f5bdd6b21a5063661c3fe79
Armin Veres <armin.veres@hotmail.com>
2024-09-02 22:50:09 +0200

    feat: add bootable hello world
    Build and run kernel and print hw to vga buffer.

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,21 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-/// .
+
+static HELLO_BUF: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO_BUF.iter().enumerate() {
+        // NOTE(aver): it would be more idiomatic, if we abstracted the vga buffer into its own
+        // entity.
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
